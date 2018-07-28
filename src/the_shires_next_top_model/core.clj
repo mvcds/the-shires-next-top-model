e1c2d89ed550a734affc114ac4324effc08a40dc
Marcos Vinícius C. da Silva <marcos.silva@chamatheapp.com>
2018-07-27 22:51:30 -0300

    use loop to symetrize

--- src/the_shires_next_top_model/core.clj
+++ src/the_shires_next_top_model/core.clj
@@ -27,3 +27,19 @@
                              {:name "left-achilles" :size 1}
                              {:name "left-foot" :size 2}])
 
+(defn matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+(defn symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts
+         final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts]
+        (recur remaining
+               (into final-body-parts
+                     (set [part (matching-part part)])))))))
