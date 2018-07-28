4de932a28d69df24f3f72e6a9dbd2339b8552c05
Marcos Vinícius C. da Silva <marcos.silva@chamatheapp.com>
2018-07-27 22:51:47 -0300

    use reduce to symetrize

--- src/the_shires_next_top_model/core.clj
+++ src/the_shires_next_top_model/core.clj
@@ -32,14 +32,10 @@
   {:name (clojure.string/replace (:name part) #"^left-" "right-")
    :size (:size part)})
 
-(defn symmetrize-body-parts
+(defn better-symmetrize-body-parts
   "Expects a seq of maps that have a :name and :size"
   [asym-body-parts]
-  (loop [remaining-asym-parts asym-body-parts
-         final-body-parts []]
-    (if (empty? remaining-asym-parts)
-      final-body-parts
-      (let [[part & remaining] remaining-asym-parts]
-        (recur remaining
-               (into final-body-parts
-                     (set [part (matching-part part)])))))))
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (matching-part part)])))
+          []
+          asym-body-parts))
